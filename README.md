e2aaac59a7d605e2c3541efd468ab278643ef65c
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2024-02-08 02:33:16 -0300

    add README

---
+++ README.md
@@ -0,0 +1,39 @@
+<h1 align="center">APP de Pedidos</h1>
+
+<p align="center">
+Aplicativo de pedidos desenvolvido com React Native no evento NLW da Rocketseat<br/>
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto python" src="./assets/images/layout.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+- React Native
+- Type Scrypt
+
+## 💻 Projeto
+
+-[Visite o projeto online]()
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](. É necessário ter conta no [Figmam) para acessá-lo.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,34 +1,34 @@
-
-import { useState } from "react"
-import { View, FlatList} from "react-native";
-import { Header } from "@/components/header"
+import { useState } from "react";
+import { View, FlatList } from "react-native";
+import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
-import {CATEGORIES}from "@/utils/data/products"
+import { CATEGORIES } from "@/utils/data/products";
 
-export default function Home(){
-    const[category,setCategory] = useState(CATEGORIES[0])
+export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
 
-    function handleCategorySelect(selectedCategory:string){
-        setCategory(selectedCategory)
-    }
-    return(
-        <View className=" flex-1 pt-8">
-         <Header title= "Faça seu Pedido" cartQuantityItems={3}/>
-            
-            <FlatList 
-            data={CATEGORIES}
-            keyExtractor={(item=>item)}
-            renderItem={({item})=>
-             <CategoryButton title={item}isSelected={item===category}
-             onPress={()=> handleCategorySelect(item)}
-             />}
-            horizontal
-            className="max-h-10 mt-5"
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle = {{gap:12, paddingHorizontal:28}}
-            />
-        
-        </View>
-    )
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+  return (
+    <View className=" flex-1 pt-8">
+      <Header title="Faça seu Pedido" cartQuantityItems={3} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 28 }}
+      />
+    </View>
+  );
 }
-
